91e784616d653292252fa77ef7bb2ac62f80ff56
Julien Roche <jroche@objetdirect.com>
2013-06-15 21:29:30 +0200

    Add service for the rest api
    Only for Tower for the moement

--- android/src/com/marsattack/service/TowerService.java
+++ android/src/com/marsattack/service/TowerService.java
@@ -2,6 +2,9 @@ package com.marsattack.service;
 
 import java.util.List;
 
+import com.marsattack.exception.RestCannotDelete;
+import com.marsattack.exception.RestCannotGetData;
+import com.marsattack.exception.RestCannotUpdateData;
 import com.marsattack.model.Tower;
 
 /**
@@ -15,25 +18,25 @@ public interface TowerService {
 	 * Delete the specified tower
 	 * @param tower Tower to delete
 	 */
-	void delete(Tower tower);
+	void delete(Tower tower) throws RestCannotDelete;
 	
 	/**
 	 * Retrieve all towers
 	 * @return the list of towers
 	 */
-	List<Tower> get();
+	List<Tower> get() throws RestCannotGetData;
 	
 	/**
 	 * Get the specified tower by its identity
 	 * @param id Identity
 	 * @return the tower, null otherwise
 	 */
-	Tower get(int id);
+	Tower get(int id) throws RestCannotGetData;
 	
 	/**
 	 * Save or update the tower
 	 * @param tower
 	 * @return the updated tower
 	 */
-	Tower save(Tower tower);
+	Tower save(Tower tower) throws RestCannotUpdateData;
 }

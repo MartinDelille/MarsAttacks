3eb68c10b8edb25845ebc063058c108c1048a60e
victor <victor@sogilis.com>
2013-06-16 00:38:47 +0200

    remove showposition everything is working now

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -93,9 +93,3 @@ function getLocation()
     }
   else{x.innerHTML="Geolocation is not supported by this browser.";}
   }
-
-function showPosition(position)
-  {
-  x.innerHTML="Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;  
-  }

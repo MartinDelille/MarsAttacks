25f7a252df3bf9b112fb64a2d8564fe716bd626a
victor <victor@sogilis.com>
2013-06-16 00:15:14 +0200

    add localization event

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -8,5 +8,21 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
-
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+// fonction to get latitude et longitude
+var x=document.getElementById("build-button");
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
+function showPosition(position)
+  {
+  x.innerHTML="Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;  
+  }

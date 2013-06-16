f32d6213f8faefae452923182156979b82abe528
Julien Roche <jroche@objetdirect.com>
2013-06-16 02:29:39 +0200

    Aliens with a 'little' ia for attacks

--- webapp/js/aliens.js
+++ webapp/js/aliens.js
@@ -1,8 +1,10 @@
 window.jQuery && window.google && (function($, google) {
     var worker,
         jCounterNextAttack,
+        jCounterAlienWin,
         centerMap,
         areaToProtect,
+        alienWin = 0,
         aliens = [];
     
     // Private functions
@@ -22,6 +24,10 @@ window.jQuery && window.google && (function($, google) {
         }, false);
     }
     
+    function refreshAlienWinCounter() {
+        jCounterAlienWin.text(alienWin);
+    }
+    
     function attack() {
         console.info("Human detecting. EXTERMINATE !!");
         
@@ -36,7 +42,6 @@ window.jQuery && window.google && (function($, google) {
             });
             
             marker.orientation = orientation;
-            
             aliens.push(marker);
         }
     }
@@ -45,33 +50,40 @@ window.jQuery && window.google && (function($, google) {
         window.requestAnimationFrame(moveAliens);
         
         for(var i = aliens.length - 1, distance; i >= 0; --i){
-            distance = google.maps.geometry.spherical.computeDistanceBetween(centerMap, aliens[i].getPosition()) - 2;
+            distance = google.maps.geometry.spherical.computeDistanceBetween(centerMap, aliens[i].getPosition()) - 5;
             aliens[i].setPosition(google.maps.geometry.spherical.computeOffset(centerMap, distance, aliens[i].orientation ));
          
-            /*if(areaToProtect.getBounds().contains(aliens[i].getPosition())){
-                aliens[i].splice(i, 1);
-            }*/
+            if(areaToProtect.getBounds().contains(aliens[i].getPosition())){
+                aliens[i].setMap(null);
+                aliens.splice(i, 1);
+                
+                alienWin++;
+                window.setTimeout(refreshAlienWinCounter, 0);
+            }
         }
     }
 
     // What to do ?
     document.addEventListener("DOMContentLoaded", function() {
         // Get some DOM elements
-        jCounterNextAttack = $("#counter-next-attack")
+        jCounterNextAttack = $("#counter-next-attack"),
+        jCounterAlienWin = $("#counter-alien-win");
         
         // Wait the DOM-Ready to do some stuff !
         google.maps.event.addDomListener(window, "load", function() {
             centerMap = map.getCenter();
             areaToProtect = new google.maps.Circle({
                 map: map,
-                position: centerMap,
-                radius: 5000,
+                center: centerMap,
+                clickable: false,
+                radius: 200,
                 fillColor: "#4096EE",
                 fillOpacity: "0.5",
                 strokeColor: "#3F4C6B",
-                strokeWeight: 5
+                strokeWeight: 2
             });
             initializeCounterNextAttack();
+            refreshAlienWinCounter();
             moveAliens();
         });
     });

8b2b711b53fdfab20da25db23d7c1c74b460f829
Lionel Armanet <lionel@Lionel-Armanets-MacBook.local>
2013-06-25 20:46:05 +0200

    Websocket to add aliens

---
+++ webapp/js/app/models/SocketBroker.js
@@ -0,0 +1,18 @@
+/**
+ * Module to broke between sockets and other components. Returns a unique getInstance
+ * function that will create the WebSocket only once.
+ */
+define(["socket.io"], function(io) {
+
+    var socket = io.connect("http://wwwdev:1337");;
+    socket.on("connected", function (data) {
+        console.log("We are connected: " + JSON.stringify(data));
+    });
+
+    return {
+      getInstance: function() {
+        return socket;
+      }
+    };    
+
+});
--- webapp/js/app/models/TowerModel.js
+++ webapp/js/app/models/TowerModel.js
@@ -1,6 +1,6 @@
 define(
-    ["backbone"], 
-    function(Backbone) {
+    ["backbone", "js/app/models/SocketBroker"], 
+    function(Backbone, SocketBroker) {
 
     // define tower and its collection
 
@@ -11,6 +11,7 @@ define(
     var Towers = Backbone.Collection.extend({
         url: '/backend/towers',
         model: Tower
+
     });
 
     return {

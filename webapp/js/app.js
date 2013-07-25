15819d23b7e634b60aa022d25c4bafccd4d6b6b7
Lionel Armanet <lionel.armanet@gmail.com>
2013-07-26 00:19:31 +0200

    Conflict (rebase)

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,6 +1,7 @@
 /**
  * Module to manager towers on the map.
  */
+
  var TowerMap = (function() {
 
   var TowerModel = function() {
@@ -59,36 +60,45 @@
   };
   TowerPinView.prototype = {
     displayMarker: function() {
+      var iconTower = 'img/tower.png';
       var googlePos = new google.maps.LatLng(this.position.latitude,this.position.longitude);
       var marker = new google.maps.Marker({
         position: googlePos,
-        map: map
+        map: map,
+        icon: iconTower
       });
     }
   };
 
-	var TowerView = function(model) {
-		this.model = model;
-	};
-	TowerView.prototype = {
-		loadTowers: function() {
-			$(this.model).on('loaded', $.proxy(this.draw, this));
-			this.model.load();
-			this.towerPins = [];
-		},
-		draw: function() {
-			var iconTower = 'img/tower.png';
-			for (var i=0; i<this.model.towers.length; i++) {
-				var tower = this.model.towers[i];
-				var marker = new google.maps.Marker({
-					position:  new google.maps.LatLng(tower.latitude, tower.longitude),
-					map: map,
-					icon: iconTower
-				});
-				this.towerPins.push(marker);				
-			}
-		}
-	};
+  var TowerView = function(model) {
+    this.model = model;
+  };
+  TowerView.prototype = {
+    loadTowers: function() {
+      $(this.model).on('loaded', $.proxy(this.draw, this));
+      this.model.load();
+      this.towerPins = [];
+    },
+    draw: function() {
+      for (var i=0; i<this.model.towers.length; i++) {
+        var tower = this.model.towers[i];
+        modelPinView = new TowerPinView(tower);
+        modelPinView.displayMarker();
+      }
+    },
+    bind: function() {
+      $("#build-tower").on('click',$.proxy(this.onBuildTower, this));
+    },
+    onBuildTower: function(e) {
+      e.preventDefault();
+      var newTower = new TowerModel();
+      $(newTower).on('created', function (e,jsonTower) {
+        var newViewTower = new TowerPinView(jsonTower);
+        newViewTower.displayMarker();
+      });
+      newTower.create();
+    }
+  };
 
   return {
     init: function() {
